7bd0d2234e7a7ce8a292610efcbfc8aa412142c0
Afaren <550047450@qq.com>
2016-08-31 19:34:00 +0800

    afaren - refactor Token class

--- src/afar/tokenizer/Token.java
+++ src/afar/tokenizer/Token.java
@@ -9,27 +9,13 @@ import java.util.regex.Pattern;
  */
 public class Token implements Serializable {
     private String token;
-    //    private String tokenType;
-    private String tokenValue;
-
-    private TokenType tokenType;
-
-
+    private String value;
+    private TokenType type;
 
     public enum TokenType {
         sign, constant, identifier, reserved
     }
 
-    /*
-        我想将类型修改为枚举，这样Parse的写法应该会简单很多才对
-     */
-    private boolean isLegalSign;
-    private boolean isLegalConstant;
-    private boolean isLegalIdentifier;
-    private boolean isLegalReservedWord;
-
-
-    //   reserved word: PROGRAM, BEGIN, END, VAR, INTEGER, WHILE, IF, THEN, ELSE, DO, PROCEDUCE
     private static ArrayList<String> reservedWordDirectory;
 
     static {
@@ -47,7 +33,6 @@ public class Token implements Serializable {
         reservedWordDirectory.add("procedure");
     }
 
-    //    sign: + - * / = <> <= >= > < ( ) := , . ; : .. ‘ ’ ^ @$
     private static ArrayList<String> signDirectory;
 
     static {
@@ -76,12 +61,87 @@ public class Token implements Serializable {
     }
 
     public Token(String str) {
-        // 初始化这个域，后面利用系列布尔函数判断其属性
         this.token = str;
-        saveDependOnType(str);
+        setType(str);
     }
 
-    private void saveDependOnType(String str) {
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+        setType(token);
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void reset() {
+        token = null;
+        type = null;
+        value = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "token='" + token + '\'' +
+                ", value='" + value + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Token token1 = (Token) o;
+
+        if (!token.equals(token1.token)) return false;
+        if (!value.equals(token1.value)) return false;
+        return type.equals(token1.type);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = token.hashCode();
+        result = 31 * result + value.hashCode();
+        result = 31 * result + type.hashCode();
+        return result;
+    }
+
+
+    public boolean isLegalToken() {
+        return isSign() || isIdentifier() || isConstant() || isReserved();
+    }
+
+    private boolean isSign() {
+        return signDirectory.contains(token);
+    }
+
+    private boolean isReserved() {
+        return reservedWordDirectory.contains(token);
+    }
+
+    private boolean isConstant() {
+        String constantRegx = "^[\\d]+[\\.]?[\\d]*$";
+        return Pattern.matches(constantRegx, token);
+    }
+
+    private boolean isIdentifier() {
+        String identifierRegx = "^[a-zA-Z][0-9a-zA-Z]*$";
+        return Pattern.matches(identifierRegx, token);
+    }
+
+    private void setType(String str) {
         if (isReserved()) {
             setAsReserved(str);
         } else if (isIdentifier()) {
@@ -94,124 +154,23 @@ public class Token implements Serializable {
     }
 
     private void setAsConstant() {
-        this.tokenValue = token;
-//        this.tokenType = "constant";
-        this.tokenType = TokenType.constant;
+        this.value = token;
+        this.type = TokenType.constant;
     }
 
-
     private void setAsSign() {
-        this.tokenValue = token;
-//        this.tokenType = "sign";
-        this.tokenType = TokenType.sign;
+        this.value = token;
+        this.type = TokenType.sign;
     }
 
     private void setAsIdentifier() {
-        this.tokenValue = token;
-//        this.tokenType = "identifier";
-        this.tokenType = TokenType.identifier;
+        this.value = token;
+        this.type = TokenType.identifier;
     }
 
     private void setAsReserved(String str) {
-        this.tokenValue = str;
-        this.tokenType = TokenType.reserved;
+        this.value = str;
+        this.type = TokenType.reserved;
     }
 
-    public TokenType getType() {
-        return tokenType;
-    }
-
-    public String getValue() {
-        return tokenValue;
-    }
-
-    @Override
-    public String toString() {
-
-        return "Token{" +
-                "token='" + token + '\'' +
-                ", tokenValue='" + tokenValue + '\'' +
-                ", tokenType='" + tokenType + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Token token1 = (Token) o;
-
-        if (!token.equals(token1.token)) return false;
-        if (!tokenValue.equals(token1.tokenValue)) return false;
-        return tokenType.equals(token1.tokenType);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = token.hashCode();
-        result = 31 * result + tokenValue.hashCode();
-        result = 31 * result + tokenType.hashCode();
-        return result;
-    }
-
-
-    public boolean isLegalToken() {
-//        check if it is a  sign or identifier or constant
-//        String constantPatter = "[1-9]*\\.[0-9]*$";
-//        [+-]?[\d]+[\.]?[\d]*$
-        if (isLegalSign || isLegalReservedWord || isLegalIdentifier || isLegalConstant) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isSign() {
-        isLegalSign = signDirectory.contains(token);
-        return isLegalSign;
-    }
-
-
-    private boolean isConstant() {
-        String constantPatter = "^[\\d]+[\\.]?[\\d]*$";
-//        String constantPatter = "^[1-9]+[\\.]?[\\d]*$";
-        isLegalConstant = Pattern.matches(constantPatter, token);
-        return isLegalConstant;
-    }
-
-
-    private boolean isIdentifier() {
-        String identifierPatter = "^[a-zA-Z][0-9a-zA-Z]*$";
-        isLegalIdentifier = Pattern.matches(identifierPatter, token);
-        return isLegalIdentifier;
-    }
-
-
-    private boolean isReserved() {
-        isLegalReservedWord = reservedWordDirectory.contains(token);
-        return isLegalReservedWord;
-    }
-
-
-    public void setToken(String token) {
-        this.token = token;
-//
-//        isReserved();
-//        isConstant();
-//        isSign();
-//        isIdentifier();
-        saveDependOnType(token);
-//        Token(token);
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void reset() {
-        token = null;
-        tokenType = null;
-        tokenValue = null;
-    }
 }
